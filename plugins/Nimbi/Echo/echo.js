7f5e67027749b7081711ec654aaca95570301220
Nimbi <Darkwater409@gmail.com>
2021-08-11 02:17:34 -0700

    Update echo.js

--- plugins/Nimbi/Echo/echo.js
+++ plugins/Nimbi/Echo/echo.js
@@ -6,7 +6,7 @@ module.exports.run = (client, message, args) => {
         .addField('Latency', `${Date.now() - message.createdTimestamp}ms.`, true)
         .addField('API Latency', `${Math.round(client.ws.ping)}ms`, true)
         .setColor('0x206694')
-        .setThumbnail('https://i.gifer.com/origin/a6/a6b6c68285b2389353c579c87ad9c1d3.gif')
+        .setThumbnail('https://cdn.dribbble.com/users/2014359/screenshots/5710863/cubes.gif')
         .setFooter('Made by Nimbi#4961', 'https://cdn.discordapp.com/avatars/465228604721201158/a_7c9eb1d6a8e7fab650a835db33b2b28f.gif?size=1024')
     message.channel.send({ embed: echo })
         ;
